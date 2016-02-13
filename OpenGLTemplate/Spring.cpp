dda5a1abbc8823909e3ae52c3831f36d8f8a248f
Kyle Larson <klarson1331@gmail.com>
2016-02-13 13:16:09 -0500

    Cleaned up Spring constructor, removed commented out code

--- OpenGLTemplate/Spring.cpp
+++ OpenGLTemplate/Spring.cpp
@@ -1,7 +1,15 @@
 #include "Spring.h"
 
 
-Spring::Spring(PhysicsComponent& nodeA, PhysicsComponent& nodeB, glm::vec3 nodeAAnchorPoint, glm::vec3 nodeBAnchorPoint, float k ) : GameObject(glm::vec3(0.0f), glm::vec3(0.0f), glm::vec3(0.0f)), a{ nodeA }, b{ nodeB }
+Spring::Spring(
+	PhysicsComponent& nodeA, 
+	PhysicsComponent& nodeB, 
+	glm::vec3 nodeAAnchorPoint, 
+	glm::vec3 nodeBAnchorPoint, 
+	float k ) 
+	: GameObject(glm::vec3(0.0f), glm::vec3(0.0f), glm::vec3(0.0f)), 
+	a{ nodeA }, 
+	b{ nodeB }
 {
 	//aLoc and bLoc are relative to the object's 0 transform.  To get world coordinates, multiply by node's transform matrices
 	aLoc = glm::vec4(nodeAAnchorPoint, 1.0f);
@@ -31,23 +39,6 @@ void Spring::update(float dt) {
 	a.GOTransform.setPosition(aState.position);
 	b.setVelocity(bState.velocity);
 	b.GOTransform.setPosition(bState.position);
-
-	/*glm::vec3 aPosition = glm::vec3(a.GOTransform.transformMatrix * aLoc);
-	glm::vec3 bPosition = glm::vec3(b.GOTransform.transformMatrix * bLoc);
-
-	glm::vec3 diff = bPosition - aPosition;
-	float displacement = glm::length(diff) - restingLength;
-	if (!std::isfinite(displacement))
-	{
-		displacement = std::numeric_limits<float>::max();
-		diff *= 1.0f / diff.x;
-	}
-
-	diff = glm::normalize(diff);
-	diff *= k * displacement * 0.5f;
-	a.addForce(diff, aPosition);
-	diff *= -1;
-	b.addForce(diff, bPosition);*/
 }
 
 void Spring::draw() {
